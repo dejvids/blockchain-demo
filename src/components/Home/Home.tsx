adc501d4512b0cc6990724f78af1099bef2b2026
dawid <dawidsurys@hotmail.com>
2021-09-23 22:56:21 +0200

    Add Pop and Clear options

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,6 +8,7 @@ import BlockModel from '../Block/Model/BlockModel';
 import Footer from '../Footer/Footer';
 import { setHash, validateBlockchain } from './BlockchainManager';
 import sha256 from 'crypto-js/sha256';
+import MainToolbar from '../MainToolbar/MainToolbar'
 
 const data = require('../../data.json');
 
@@ -30,7 +31,7 @@ const Home: React.FC = () => {
 
   const onAddClicked = () => {
     const newBlock = new BlockModel(blockchain.blocks[0].height + 1, Date.now(), blockchain.blocks[0].hash, '');
-    
+
 
     const newBlockchain: Blockchain = { ...blockchain, blocks: [newBlock, ...blockchain.blocks] };
 
@@ -38,48 +39,57 @@ const Home: React.FC = () => {
     setBlockchain(newBlockchain);
   };
 
-  const onBlockUpdate = (newBlock: IBlockModel) =>{
-    const newBlockchain: Blockchain = {...blockchain, blocks: [...blockchain.blocks]};
+  const onBlockUpdate = (newBlock: IBlockModel) => {
+    const newBlockchain: Blockchain = { ...blockchain, blocks: [...blockchain.blocks] };
     newBlockchain.blocks[blockchain.blocks.length - newBlock.height] = newBlock;
-  
-    validate(newBlockchain);
+
     updateBlckStatus(newBlockchain, newBlock.height);
     setBlockchain(newBlockchain);
   }
 
-  const validate = (bc: Blockchain) => {
-     //const validationResult = validateBlockchain(bc);
-    // if(validationResult > -1)
-    // {
-    //   console.log('Invalid blockchain starting from block: ' + validationResult)
-    //   let i = 0;
-    //   while(bc.blocks[i].height >= validationResult) {
+  const onClearClicked = () => {
+    let newBlockchain = { ...blockchain, blocks: [originBLock] }
 
-    //     bc.blocks[i].isValid = false;
-    //     i++;
-    //   }
-    // }
+    setBlockchain(newBlockchain);
+  }
+
+  const onPopClicked = () => {
+    if(blockchain.blocks.length <=1)
+      return;
+      
+    let blocks = blockchain.blocks.splice(1);
+
+    let newBlockchain = {...blockchain, blocks: blocks}
+    setBlockchain(newBlockchain);
   }
 
   const updateBlckStatus = (bc: Blockchain, blockHeight: number) => {
-    let i = bc.blocks.indexOf(bc.blocks.filter(x=>x.height == blockHeight)[0])
-    
-    while(i >0) {
-      bc.blocks[i-1].isValid = bc.blocks[i-1].prevBlockHash === bc.blocks[i].hash && bc.blocks[i].isValid;
+    let i = bc.blocks.indexOf(bc.blocks.filter(x => x.height == blockHeight)[0])
+
+    while (i > 0) {
+      bc.blocks[i - 1].isValid = bc.blocks[i - 1].prevBlockHash === bc.blocks[i].hash && bc.blocks[i].isValid;
       i--;
     }
-    
-    if(i == 0 && bc.blocks.length > 1) {
+
+    if (i == 0 && bc.blocks.length > 1) {
       bc.blocks[i].isValid = bc.blocks[0].prevBlockHash == bc.blocks[1].hash && bc.blocks[1].isValid;
     }
   }
 
   return (
     <div className={styles.container}>
-      <button className="btn-add" onClick={onAddClicked}>Add</button>
+      <MainToolbar addBtnClicked={onAddClicked} popBtnClicked={onPopClicked} clearBtnClicked={onClearClicked}/>
       <ul>
         {blockchain.blocks.map((value, id) => {
-          return (<Block key={id} blockModel={value} onBlockUpdate={onBlockUpdate}/>);
+          return (
+            <div>
+            
+           {/* <svg height="200" width="200">
+            <line x1="0" y1="0" x2="100" y2="100" stroke="red" stroke-width="2"/>
+          </svg> */}
+            <Block key={id} blockModel={value} onBlockUpdate={onBlockUpdate} />
+          </div>
+          );
         })}
       </ul>
     </div>
---
+++ src/components/MainToolbar/MainToolbar.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import './MainToolbar.css'
+
+
+interface MainToolbarProps {
+    addBtnClicked : () => void;
+    popBtnClicked? :() => void;
+    clearBtnClicked?: () => void;
+}
+const MainToolbar: React.FC<MainToolbarProps> = ({addBtnClicked, popBtnClicked, clearBtnClicked}) => {
+
+    const onAddClicked = () => {
+        addBtnClicked();
+    }
+
+    const onClearClicked = () => {
+        if(clearBtnClicked)
+            clearBtnClicked();
+    }
+
+    const onPopClicked = () => {
+        if(popBtnClicked)
+            popBtnClicked();
+    }
+
+    return (
+        <div className="toolbar">
+            <button className="btn-toolbar btn-add" onClick={onAddClicked}>Add</button>
+            <button className="btn-toolbar btn-pop" onClick={onPopClicked}>Pop</button>
+            <button className="btn-toolbar btn-clear" onClick={onClearClicked}>Clear</button>
+        </div>
+    )
+}
+
+export default MainToolbar
